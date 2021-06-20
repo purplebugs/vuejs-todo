7356dc24260e10f9b164833b9dde2468cfb6c299
Anita Lipsky <anita@purplebugs.com>
2021-06-20 13:17:58 +0200

    add data property for tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <v-list flat>
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active" color="primary"></v-checkbox>
-          </v-list-item-action>
+    <v-list class="pt-0" flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>Create Todo app</v-list-item-title>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
     </v-list>
   </div>
 </template>
@@ -19,5 +22,20 @@
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 0,
+          title: "Create Todo app",
+        },
+        { id: 1, title: "Get caught in the rain" },
+        {
+          id: 2,
+          title: 'Get Vue.js app "re-VUE-ed" by peers',
+        },
+      ],
+    };
+  },
 };
 </script>
