c2ce5b4ce6ef7e9624960751f8167e155da921a8
Anita Lipsky <anita@purplebugs.com>
2021-06-20 12:38:08 +0200

    get pages ready

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About this Vuetify Todo app</h1>
+    <p>
+      This is Anita Lipsky's Vue.js v2 practise project for the Frontend
+      interview task
+    </p>
   </div>
 </template>
