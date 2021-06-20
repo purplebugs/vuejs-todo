edf52133da7c8b2f4a4ddb2ac6a6d8a6ad32f091
Anita Lipsky <anita@purplebugs.com>
2021-06-20 12:27:40 +0200

    hook up navigation

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -34,7 +34,7 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -44,8 +44,8 @@ export default {
   data: () => ({
     drawer: null,
     items: [
-      { title: "Todo", icon: "mdi-view-dashboard" },
-      { title: "About", icon: "mdi-help-box" },
+      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
     ],
   }),
 };
