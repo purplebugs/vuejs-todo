8c8b0adf93292dec7ccf0baf742ae1ff0d1c62fe
Anita Lipsky <anita@purplebugs.com>
2021-06-20 13:03:44 +0200

    Create a list of tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo page</h1>
+  <div class="home">
+    <v-list flat>
+      <v-list-item>
+        <template v-slot:default="{ active }">
+          <v-list-item-action>
+            <v-checkbox :input-value="active" color="primary"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Create Todo app</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
