9a6328e8c360578eb570d2d0f81c15aeca794df6
Anita Lipsky <anita@purplebugs.com>
2021-07-09 16:18:17 +0200

    show prompt to delete dialog when click delete task

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -17,18 +17,30 @@
         </v-list-item-content>
 
         <v-list-item-action>
-          <v-btn @click.stop="$store.dispatch('deleteTask', task.id)" icon>
+          <v-btn @click.stop="dialogs.delete = true" icon>
             <v-icon color="primary lighten-1">mdi-delete</v-icon>
           </v-btn>
         </v-list-item-action>
       </template>
     </v-list-item>
     <v-divider></v-divider>
+    <dialog-delete v-if="dialogs.delete" />
   </div>
 </template>
 
 <script>
 export default {
   props: ["task"],
+  data() {
+    return {
+      dialogs: {
+        delete: false,
+      },
+    };
+  },
+  components: {
+    "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
+      .default,
+  },
 };
 </script>
