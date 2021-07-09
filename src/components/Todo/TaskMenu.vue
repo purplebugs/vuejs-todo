12a9e6216ace56b5a1f55e3dd6736191751cd8b2
Anita Lipsky <anita@purplebugs.com>
2021-07-09 17:11:27 +0200

    add menu items and icons

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -8,6 +8,9 @@
 
     <v-list>
       <v-list-item v-for="(item, i) in items" :key="i">
+        <v-list-item-icon>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-icon>
         <v-list-item-title>{{ item.title }}</v-list-item-title>
       </v-list-item>
     </v-list>
@@ -18,10 +21,18 @@
 export default {
   data: () => ({
     items: [
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me 2" },
+      {
+        title: "Edit",
+        icon: "mdi-pencil",
+      },
+      {
+        title: "Due date",
+        icon: "mdi-calendar",
+      },
+      {
+        title: "Delete",
+        icon: "mdi-delete",
+      },
     ],
   }),
 };
