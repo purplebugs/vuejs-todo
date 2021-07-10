b32d05deb47b3e1e543b39295c92bbc7dd4649e7
Anita Lipsky <anita@purplebugs.com>
2021-07-10 18:37:06 +0200

    add snackbar when task updated

--- src/store/index.js
+++ src/store/index.js
@@ -78,6 +78,10 @@ export default new Vuex.Store({
       commit("deleteTask", id);
       commit("showSnackbar", "Task deleted!");
     },
+    updateTaskTitle({ commit }, payload) {
+      commit("updateTaskTitle", payload);
+      commit("showSnackbar", "Task updated!");
+    },
   },
   modules: {},
 });
