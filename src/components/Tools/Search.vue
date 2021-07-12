fcdac6c0d2b4cef554990848825030dc1e432d90
Anita Lipsky <anita@purplebugs.com>
2021-07-12 16:00:17 +0200

    only close search if empty, show cursor if hover over search image

--- src/components/Tools/Search.vue
+++ src/components/Tools/Search.vue
@@ -1,9 +1,10 @@
 <template>
   <v-text-field
+    v-model="search"
     @focus="searchClosed = false"
     @blur="searchClosed = true"
     class="expanding-search mt-1"
-    :class="{ closed: searchClosed }"
+    :class="{ closed: searchClosed && !search }"
     placeholder="Search"
     filled
     dense
@@ -16,6 +17,7 @@
 export default {
   data() {
     return {
+      search: null,
       searchClosed: true,
     };
   },
@@ -25,6 +27,8 @@ export default {
 <style lang="sass">
 .expanding-search
   transition: max-width 0.3s
+  .v-input__slot
+    cursor: pointer
   &.closed
     max-width: 45px
 </style>
